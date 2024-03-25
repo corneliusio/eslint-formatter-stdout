9b9e1b88cf5da7dd6a753c948e28b1d931568acb
Cornelius Ukena <guy@cornelius.dev>
2024-03-25 15:51:38 -0500

    Gotta get the og file myself if no fixes i guess

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-export default results => results.map(({ output }) => output && output.trim()).join(`\0`)
+import fs from 'node:fs'
+
+export default results => {
+    return results.map(result => {
+        if (result.output) {
+            return result.output.trim()
+        }
+
+        return fs.readFileSync(result.filePath, 'utf8')
+    }).join(`\0`).trim()
+}
